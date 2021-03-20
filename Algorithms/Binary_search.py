31b1a2414f77b438e793fa4840c7fd077f672d1e
eduardo-conteras-dominguez <edu.contreras@gmail.com>
2021-03-20 12:10:37 +0100

    Implemented binary search with repetitions allowed in sorted list.

--- Algorithms/Binary_search.py
+++ Algorithms/Binary_search.py
@@ -36,8 +36,36 @@ def binary_search_recursive(numbers_list, number_to_find, left_index, right_inde
     return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
 
 
-if __name__ == "__main__":
+def binary_search_with_repetitions(number_list, number_to_find):
+    """
+    Binary search de una lista ordenada con repeticiones.
+    :param number_list:
+    :param number_to_find:
+    :return: Lista de los indices en los que el numero se halla.
+    """
+    left_index = 0
+    right_index = len(number_list) - 1
+    mid_index = 0
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = number_list[mid_index]
+        if mid_number == number_to_find:
+            index_search_left = index_search_right = mid_index
+            while number_list[index_search_left] == number_to_find:
+                index_search_left -= 1
+            index_search_left += 1
+            while number_list[index_search_right] == number_to_find:
+                index_search_right += 1
+            index_search_right -= 1
+            return [i for i in range(index_search_left, index_search_right + 1)]
+        if number_to_find > mid_number:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+    return -1
 
+
+if __name__ == "__main__":
     sorted_list = [i for i in range(10001)]
     print("Case number isn't in the list: Use number -1")
     print("normal binary search: ")
@@ -59,5 +87,8 @@ if __name__ == "__main__":
     print(binary_search(sorted_list, 10000))
     print("recursive binary search: ")
     print(binary_search_recursive(sorted_list, 10000, 0, len(sorted_list)))
-
-
+    print("NOW WITH REPETITIONS: ")
+    sorted_list = [1, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 12]
+    print(sorted_list)
+    print("Let us search repeated number: 4")
+    print(binary_search_with_repetitions(sorted_list, 4))
