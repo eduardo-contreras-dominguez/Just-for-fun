ae69c7a5b80c4442af4d0a212fb6458b00350c6d
educontreras <edu.contreras@gmail.com>
2022-12-10 12:22:35 +0100

    Added Flipping Matrix code, answer.

--- HackerRank/FlippingMatrix/FlippingMatrix.py
+++ HackerRank/FlippingMatrix/FlippingMatrix.py
@@ -1,1 +1,18 @@
+def FlippingMatrix(matrix):
+    """
 
+    :param matrix: List of lists. Representing a 2n x 2n matrix.
+
+    :return: Maximum sum.
+    """
+    n = len(matrix)
+    output = 0
+    for i in range(n // 2):
+        for j in range(n // 2):
+            output += max([matrix[i][j], matrix[i][n - j - 1], matrix[n - i - 1][j], matrix[n - i - 1][n - j - 1]])
+    return output
+
+
+if __name__ == "__main__":
+    s = FlippingMatrix([[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]])
+    print("Maximal sum on the upper left submatrix, ", s)
